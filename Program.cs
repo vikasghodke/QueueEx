e696d517ffc24576939fa86688dd50b599a0f2be
Vikas ghodke <vikasghodke468@gmail.com>
2024-02-02 16:59:51 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackPractice
+{
+    internal class Maintest
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q1 = new Queue<int>(8);
+            q1.Enqueue(1);
+            q1.Enqueue(3);
+            q1.Enqueue(5);
+            q1.Enqueue(6);
+            q1.Enqueue(7);
+            q1.Dequeue();
+            q1.Dequeue();
+
+            while(q1.Count>0)
+            {
+                Console.WriteLine(q1.Dequeue());
+            }
+           
+
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueDS
+{
+    public class Queue
+    {
+        private int[] arr;
+        private int capacity;
+        private int front;
+        private int rear;
+        private int size;
+
+        public Queue(int capacity)
+        {
+            capacity = capacity;
+            arr = new int[capacity];
+            front = 0;
+            rear = -1;
+            size = 0;
+        }
+        public void Enqueue(int item)
+        {
+            if (size == capacity)
+            {
+                Console.WriteLine("Queue is full");
+                return;
+            }
+            else
+            {
+                rear = (rear + 1) % capacity;
+                arr[rear] = item;
+                size++;
+            }
+        }
+        public int Dequeu(int item)
+        {
+            if(size==0)
+            {
+                Console.WriteLine("Queue is empty");
+                return-1;
+            }
+            else
+            {
+                int dequeitem = arr[front];
+                front = (front + 1) % capacity;
+                size--;
+                return dequeitem;
+            }
+        }
+       
+
+
+    }
+}
